b2a300bae19775a12f10824865ef265162c2be4a
Daniel Siepmann <coding@daniel-siepmann.de>
2016-12-15 09:33:56 +0100

    TASK: Adjust TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'be',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-8.99.99',
+            'typo3' => '6.2.0-6.2.99',
             'php' => '5.6.0-7.99.99'
         ],
         'conflicts' => [],
