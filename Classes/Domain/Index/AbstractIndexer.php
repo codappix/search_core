3f5273f0fe2b5ba955ddf04801972cf191f16b95
Benjamin Serfhos <benjamin@serfhos.com>
2018-10-22 17:28:49 +0200

    [BUGFIX] Make sure the while loop is not closed when filterRecordsByRootLineBlacklist() returns no results

--- Classes/Domain/Index/AbstractIndexer.php
+++ Classes/Domain/Index/AbstractIndexer.php
@@ -149,8 +149,10 @@ abstract class AbstractIndexer implements IndexerInterface
         $offset = 0;
         $limit = $this->getLimit();
 
-        while (($records = $this->getRecords($offset, $limit)) !== []) {
-            yield $records;
+        while (($records = $this->getRecords($offset, $limit)) !== null) {
+            if (!empty($records)) {
+                yield $records;
+            }
             $offset += $limit;
         }
     }
