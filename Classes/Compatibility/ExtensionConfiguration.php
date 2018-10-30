5acdd918653597cd267dceeff9f4fc60c34c9d55
Daniel Maier <dm@bergwerk.ag>
2018-10-30 13:02:34 +0100

    [fix] v9 compatible extension configuration implementation

--- Classes/Compatibility/ExtensionConfiguration.php
+++ Classes/Compatibility/ExtensionConfiguration.php
@@ -2,6 +2,26 @@
 
 namespace Codappix\SearchCore\Compatibility;
 
-class ExtensionConfiguration extends \TYPO3\CMS\Core\Configuration\ExtensionConfiguration implements ExtensionConfigurationInterface
+use Codappix\SearchCore\Bootstrap;
+
+class ExtensionConfiguration implements ExtensionConfigurationInterface
 {
+    /**
+     * @return object|\TYPO3\CMS\Core\Configuration\ExtensionConfiguration
+     */
+    protected function _base()
+    {
+        return Bootstrap::getObjectManager()->get(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class);
+    }
+
+    /**
+     * @param $extensionKey
+     * @return mixed
+     * @throws \TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException
+     * @throws \TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException
+     */
+    public function get($extensionKey)
+    {
+        return $this->_base()->get($extensionKey);
+    }
 }
