7270ae0035c64550e40beb0949b4eccbf3f922e8
Daniel Siepmann <coding@daniel-siepmann.de>
2019-05-05 10:14:07 +0200

    Adjust TYPO3 constraints also in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
             'php' => '7.0.0-7.2.99'
         ],
         'conflicts' => [],
